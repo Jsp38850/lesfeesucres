07a23fcaf2827a9bb3f67c9b7e638e66a7eac195
Maxence <Balassem.mb@gmail.com>
2021-03-19 16:33:51 +0100

    Test 3 mail

--- conseillères/Les_gourmandises_de_Tif/php/contact.php
+++ conseillères/Les_gourmandises_de_Tif/php/contact.php
@@ -1,8 +1,8 @@
 <?php 
 $nom=$HTTP_POST_VARS['nom']; 
-$mail=$HTTP_POST_VARS['mail']; 
-$objet=$HTTP_POST_VARS['objet']; 
+$mail=$HTTP_POST_VARS['mail'];  
 $message=$HTTP_POST_VARS['message']; 
+$choix=$HTTP_POST_VARS['choix'];
 
 /////voici la version Mine 
 $headers = "MIME-Version: 1.0\r\n"; 
@@ -12,16 +12,16 @@ $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
 
 ////ici on détermine l'expediteur et l'adresse de réponse 
 $headers .= "From: $nom <$mail>\r\nReply-to : $nom <$mail>\nX-Mailer:PHP"; 
-
-$subject="$objet"; 
+ 
 $destinataire="balassem.mb@gmail.com"; //remplacez "webmaster@votre-site.com" par votre adresse e-mail
-$body="$message"; 
-if (mail($destinataire,$subject,$body,$headers)) { 
+$body="$message" ; 
+$body="$choix";
+if (mail($destinataire,$body,$headers)) { 
 echo "Votre mail a été envoyé<br>"; 
 } else { 
 echo "Une erreur s'est produite"; 
 } 
 ?></p>
-<p align="center">Vous allez bientot etre redirigé vers la page d'acceuil<br>
+<p>Vous allez bientot etre redirigé vers la page d'acceuil<br>
 Si vous n'etes pas redirigé au bout de 5 secondes cliquez <a href="http://www.votre-site.com">ici 
 </a></p>
