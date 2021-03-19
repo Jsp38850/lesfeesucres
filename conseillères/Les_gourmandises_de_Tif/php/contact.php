e310e24bd4d61ef9cd8a4a1b98e485ae278581ed
Maxence <Balassem.mb@gmail.com>
2021-03-19 21:06:21 +0100

    Mise a jour lien catalogue

--- conseillères/Les_gourmandises_de_Tif/php/contact.php
+++ conseillères/Les_gourmandises_de_Tif/php/contact.php
@@ -1,27 +1,130 @@
-<?php 
-$nom=$HTTP_POST_VARS['nom']; 
-$mail=$HTTP_POST_VARS['mail'];  
-$message=$HTTP_POST_VARS['message']; 
-$choix=$HTTP_POST_VARS['choix'];
-
-/////voici la version Mine 
-$headers = "MIME-Version: 1.0\r\n"; 
-
-//////ici on détermine le mail en format text 
-$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n"; 
-
-////ici on détermine l'expediteur et l'adresse de réponse 
-$headers .= "From: $nom <$mail>\r\nReply-to : $nom <$mail>\nX-Mailer:PHP"; 
+<?php
+/*
+	********************************************************************************************
+	CONFIGURATION
+	********************************************************************************************
+*/
+// expéditeur du dormulaire. Pour des raisons de sécurité, de plus en plus d'hébergeurs imposent que ce soit une adresse sur votre hébergement/nom de domaine.
+// Par exemple si vous mettez ce script sur votre site "test-site.com", mettez votre email @test-site.com comme expéditeur (par exemple contact@test-site.com)
+// Si vous ne changez pas cette variable, vous risquez de ne pas recevoir de formulaire.
+$email_expediteur = 'contact@les-fees-sucrees.fr';
+$nom_expediteur = 'Contact Les gourmandises de tif.fr';
  
-$destinataire="balassem.mb@gmail.com"; //remplacez "webmaster@votre-site.com" par votre adresse e-mail
-$body="$message" ; 
-$body="$choix";
-if (mail($destinataire,$body,$headers)) { 
-echo "Votre mail a été envoyé<br>"; 
-} else { 
-echo "Une erreur s'est produite"; 
-} 
-?></p>
-<p>Vous allez bientot etre redirigé vers la page d'acceuil<br>
-Si vous n'etes pas redirigé au bout de 5 secondes cliquez <a href="http://www.votre-site.com">ici 
-</a></p>
+// destinataire est votre adresse mail (cela peut être la même que cl'expéditeur ci-dessus). Pour envoyer à plusieurs destinataires à la fois, séparez-les par un point-virgule
+$destinataire = 'balassem.mb@gmail.com';
+ 
+// copie ? (envoie une copie au visiteur)
+$copie = 'oui'; // 'oui' ou 'non'
+ 
+// Messages de confirmation du mail
+$message_envoye = "Votre message nous est bien parvenu !";
+$message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
+ 
+// Messages d'erreur du formulaire
+$message_erreur_formulaire = "Vous devez d'abord <a href=\"contact.html\">envoyer le formulaire</a>.";
+$message_formulaire_invalide = "Vérifiez que tous les champs soient bien remplis et que l'email soit sans erreur.";
+ 
+/*
+	********************************************************************************************
+	FIN DE LA CONFIGURATION
+	********************************************************************************************
+*/
+ 
+// on teste si le formulaire a été soumis
+if (!isset($_POST['envoi']))
+{
+	// formulaire non envoyé
+	echo '<p>'.$message_erreur_formulaire.'</p>'."\n";
+}
+else
+{
+	/*
+	 * cette fonction sert à nettoyer et enregistrer un texte
+	 */
+	function Rec($text)
+	{
+		$text = htmlspecialchars(trim($text), ENT_QUOTES);
+		if (1 === get_magic_quotes_gpc())
+		{
+			$text = stripslashes($text);
+		}
+ 
+		$text = nl2br($text);
+		return $text;
+	};
+ 
+	/*
+	 * Cette fonction sert à vérifier la syntaxe d'un email
+	 */
+	function IsEmail($email)
+	{
+		$value = preg_match('/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/', $email);
+		return (($value === 0) || ($value === false)) ? false : true;
+	}
+ 
+	// formulaire envoyé, on récupère tous les champs.
+	$nom     = (isset($_POST['nom']))     ? Rec($_POST['nom'])     : '';
+	$email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
+	$objet   = (isset($_POST['objet']))   ? Rec($_POST['objet'])   : '';
+	$message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
+  $choix = (isset($_POST['choix'])) ? Rec($_POST['choix']) : '';
+	// On va vérifier les variables et l'email ...
+	$email = (IsEmail($email)) ? $email : ''; // soit l'email est vide si erroné, soit il vaut l'email entré
+ 
+	if (($nom != '') && ($email != '') && ($objet != '') && ($message != '') && ($choix != ''))
+	{
+		// les 4 variables sont remplies, on génère puis envoie le mail
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'From:'.$nom_expediteur.' <'.$email_expediteur.'>' . "\r\n" .
+				'Reply-To:'.$email. "\r\n" .
+				'Content-Type: text/plain; charset="utf-8"; DelSp="Yes"; format=flowed '."\r\n" .
+				'Content-Disposition: inline'. "\r\n" .
+				'Content-Transfer-Encoding: 7bit'." \r\n" .
+				'X-Mailer:PHP/'.phpversion();
+ 
+		// envoyer une copie au visiteur ?
+		if ($copie == 'oui')
+		{
+			$cible = $destinataire.';'.$email;
+		}
+		else
+		{
+			$cible = $destinataire;
+		};
+ 
+		// Remplacement de certains caractères spéciaux
+		$caracteres_speciaux     = array('&#039;', '&#8217;', '&quot;', '<br>', '<br />', '&lt;', '&gt;', '&amp;', '…',   '&rsquo;', '&lsquo;');
+		$caracteres_remplacement = array("'",      "'",        '"',      '',    '',       '<',    '>',    '&',     '...', '>>',      '<<'     );
+ 
+		$objet = html_entity_decode($objet);
+		$objet = str_replace($caracteres_speciaux, $caracteres_remplacement, $objet);
+ 
+		$message = html_entity_decode($message);
+		$message = str_replace($caracteres_speciaux, $caracteres_remplacement, $message);
+ 
+		// Envoi du mail
+		$cible = str_replace(',', ';', $cible); // antibug : j'ai vu plein de forums où ce script était mis, les gens ne font pas attention à ce détail parfois
+		$num_emails = 0;
+		$tmp = explode(';', $cible);
+		foreach($tmp as $email_destinataire)
+		{
+			if (mail($email_destinataire, $objet, $message, $headers))
+				$num_emails++;
+		}
+ 
+		if ((($copie == 'oui') && ($num_emails == 2)) || (($copie == 'non') && ($num_emails == 1)))
+		{
+			echo '<p>'.$message_envoye.'</p>';
+		}
+		else
+		{
+			echo '<p>'.$message_non_envoye.'</p>';
+		};
+	}
+	else
+	{
+		// une des 3 variables (ou plus) est vide ...
+		echo '<p>'.$message_formulaire_invalide.' <a href="https://www.les-fees-sucrees.fr/conseill%C3%A8res/Les_gourmandises_de_Tif/Accueil.html">Retour au formulaire</a></p>'."\n";
+	};
+}; // fin du if (!isset($_POST['envoi']))
+?>
